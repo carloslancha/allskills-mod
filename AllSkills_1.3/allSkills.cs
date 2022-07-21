538f75d403498f1a729cd17568e11cb6a9d2274f
Ezequiel A Stanganelli <ezequielstanganelli@gmail.com>
2022-07-21 00:19:25 -0300

    Re-organizing classes

--- AllSkills_1.3/allSkills.cs
+++ AllSkills_1.3/allSkills.cs
@@ -1,117 +1,71 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using Modding;
+using System;
 
-namespace AllSkills
-{
-    public class AllSkills : Mod 
-    {
-        private const string version = "1.5";
-        
-        private int? _dashLevel = null;
-        private int? _dreamNailLevel = null;
-        private bool? _dreamNailUpgraded = null;
-        private int? _fireballLevel = null;
-        private bool? _hasAcidArmour = null;
-        private bool? _hasCyclone = null;
-        private bool? _hasDash = null;
-        private bool? _hasDashSlash = null;
-        private bool? _hasDoubleJump = null;
-        private bool? _hasDreamNail = null;
-        private bool? _hasShadowDash = null;
-        private bool? _hasSuperDash = null;
-        private bool? _hasUpwardSlash = null;
-        private bool? _hasWalljump = null;
-        private int? _quakeLevel = null;
-        private int? _screamLevel = null;
 
-        public override void Initialize()
-        {
-            ModHooks.Instance.SetPlayerBoolHook += PlayerBoolSet;
+namespace AllSkills {
+    public class AllSkills {
+        private int? _dashLevel, _dreamNailLevel, _fireballLevel, _quakeLevel, _screamLevel;
+        private bool? _dreamNailUpgraded, _hasAcidArmour, _hasCyclone, _hasDash, _hasDashSlash, _hasDoubleJump, _hasDreamNail, _hasShadowDash, _hasSuperDash, _hasUpwardSlash, _hasWalljump;
+        public AllSkills() {
+            _dashLevel         = null;
+            _dreamNailLevel    = null;
+            _dreamNailUpgraded = null;
+            _fireballLevel     = null;
+            _hasAcidArmour     = null;
+            _hasCyclone        = null;
+            _hasDash           = null;
+            _hasDashSlash      = null;
+            _hasDoubleJump     = null;
+            _hasDreamNail      = null;
+            _hasShadowDash     = null;
+            _hasSuperDash      = null;
+            _hasUpwardSlash    = null;
+            _hasWalljump       = null;
+            _quakeLevel        = null;
+            _screamLevel       = null;
         }
-
-        public void PlayerBoolSet(string target, bool value)
-        {
-            PlayerData.instance.SetBoolInternal(target, value);
-            UpdateSkillValues();
-        }
-
-        public override string GetVersion()
-        {
-            return version;
-        }
-
-        private bool HasSkillNewValue()
-        {
+        public bool HasSkillNewValue() {
             PlayerData pd = PlayerData.instance;
 
             return 
                    _dreamNailUpgraded != pd.dreamNailUpgraded ||
-                   _fireballLevel != pd.fireballLevel ||
-                   _hasAcidArmour != pd.hasAcidArmour ||
-                   _hasCyclone != pd.hasCyclone ||
-                   _hasDashSlash != pd.hasDashSlash ||
-                   _hasDoubleJump != pd.hasDoubleJump ||
-                   _hasDreamNail != pd.hasDreamNail ||
-                   _hasShadowDash != pd.hasShadowDash ||
-                   _hasSuperDash != pd.hasSuperDash ||
-                   _hasUpwardSlash != pd.hasUpwardSlash ||
-                   _hasWalljump != pd.hasWalljump ||
-                   _quakeLevel != pd.quakeLevel ||
-                   _screamLevel != pd.screamLevel ||
-                   _hasDash != pd.hasDash;
+                   _fireballLevel     != pd.fireballLevel ||
+                   _hasAcidArmour     != pd.hasAcidArmour ||
+                   _hasCyclone        != pd.hasCyclone ||
+                   _hasDashSlash      != pd.hasDashSlash ||
+                   _hasDoubleJump     != pd.hasDoubleJump ||
+                   _hasDreamNail      != pd.hasDreamNail ||
+                   _hasShadowDash     != pd.hasShadowDash ||
+                   _hasSuperDash      != pd.hasSuperDash ||
+                   _hasUpwardSlash    != pd.hasUpwardSlash ||
+                   _hasWalljump       != pd.hasWalljump ||
+                   _quakeLevel        != pd.quakeLevel ||
+                   _screamLevel       != pd.screamLevel ||
+                   _hasDash           != pd.hasDash;
         }
-
-        private void UpdateSkillValues()
-        {
-            if (HasSkillNewValue())
-            {
+        public void UpdateSkillValues() {
+            if (HasSkillNewValue()) {
                 PlayerData pd = PlayerData.instance;
 
-                _dashLevel = pd.hasShadowDash ? 2 : pd.hasDash ? 1 : 0;
-                _dreamNailLevel = pd.dreamNailUpgraded ? 2 : pd.hasDreamNail ? 1 : 0;
+                _dashLevel         = pd.hasShadowDash ? 2 : pd.hasDash ? 1 : 0;
+                _dreamNailLevel    = pd.dreamNailUpgraded ? 2 : pd.hasDreamNail ? 1 : 0;
                 _dreamNailUpgraded = pd.dreamNailUpgraded;
-                _fireballLevel = pd.fireballLevel;
-                _hasAcidArmour = pd.hasAcidArmour;
-                _hasCyclone = pd.hasCyclone;
-                _hasDash = pd.hasDash;
-                _hasDashSlash = pd.hasUpwardSlash;
-                _hasDreamNail = pd.hasDreamNail;
-                _hasDoubleJump = pd.hasDoubleJump;
-                _hasShadowDash = pd.hasShadowDash;
-                _hasSuperDash = pd.hasSuperDash;
-                _hasUpwardSlash = pd.hasDashSlash;
-                _hasWalljump = pd.hasWalljump;
-                _quakeLevel = pd.quakeLevel;
-                _screamLevel = pd.screamLevel;
-                
-                WriteHtmlDataFile();
+                _fireballLevel     = pd.fireballLevel;
+                _hasAcidArmour     = pd.hasAcidArmour;
+                _hasCyclone        = pd.hasCyclone;
+                _hasDash           = pd.hasDash;
+                _hasDashSlash      = pd.hasUpwardSlash;
+                _hasDreamNail      = pd.hasDreamNail;
+                _hasDoubleJump     = pd.hasDoubleJump;
+                _hasShadowDash     = pd.hasShadowDash;
+                _hasSuperDash      = pd.hasSuperDash;
+                _hasUpwardSlash    = pd.hasDashSlash;
+                _hasWalljump       = pd.hasWalljump;
+                _quakeLevel        = pd.quakeLevel;
+                _screamLevel       = pd.screamLevel;
             }
         }
-        
-        public void WriteHtmlDataFile()
-        {
-            string path = Directory.GetCurrentDirectory() + @"\hollow_knight_Data\Managed\Mods\AllSkills_1.3";
-            string fileName = "data.html";
-            string fullPath = path + "\\" + fileName;
-            string timeString = DateTime.Now.ToString("hh:mm:ss");
-
-            // Create folder if doesn't exist 
-            System.IO.Directory.CreateDirectory(path);
-
-            try    
-            {    
-                // Check if file already exists. If yes, delete it.     
-                if (File.Exists(fullPath))    
-                {    
-                    File.Delete(fullPath);    
-                }    
-    
-                // Create a new file     
-                using (FileStream fs = File.Create(fullPath))
-                {
-                    string html = $"<html><body><script>" +
+        public string toString() {
+            string html = $"<html><body><script>" +
                                   $"const data = {{" +
                                   $"skills: {{" +
                                       $"acidArmour: {_hasAcidArmour.ToString().ToLower()}," +
@@ -132,14 +86,7 @@ namespace AllSkills
                                   $"}};" +
                                   $"parent.postMessage(data, \"*\");" +
                                   $"</script></body></html>";
-                    
-                    fs.Write(new UTF8Encoding(true).GetBytes(html), 0, html.Length);
-                }
-            }    
-            catch (Exception Ex)    
-            {    
-                Log(Ex.ToString());    
-            }
+            return html;
         }
     }
 }
